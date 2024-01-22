03366d4d6d8a6779a33baf20229f27349d19c517
surendrabodapati <surendrabodapati1@gmail.com>
2024-01-22 14:44:55 -0800

    MongoDB with different validations

--- MongoDB_5/index.js
+++ MongoDB_5/index.js
@@ -6,10 +6,15 @@ mongoose.connect('mongodb://127.0.0.1/testDb').then(()=>console.log('Connection 
 ).catch(err=> console.error('Could not connect to the MongoDB'))
 
 //schema is like defining the properties
-
+//and also writing the custom validator 
 const carSchema = mongoose.Schema({
-    manufacture : String,
+    manufacture : {type : String, minlength :4, maxlength : 50, required : true},// you can also write the fuction to return the boolean values as per the function logic, swells right!!, so can basically do many things like enum try to experiment to know better
     model : String,
+    combustionType : {type : String, required : true, enum : ["electric", 'gas', 'diesel', 'petrol','hybrid']},
+    saftyApprovals : {type : Array, validate : {
+        validator : function(saftyApprovals){
+            return saftyApprovals.length >1}
+    }},
     releaseDate : {type : Date, default : Date.now},
     isReleased : Boolean
 })
@@ -19,20 +24,30 @@ const carSchema = mongoose.Schema({
 const Car = mongoose.model('Cars', carSchema)
 
 const car = new Car({
-    manufacture : "Rangerover",
-    model : "Regular",
+    manufacture : 'Land Rover',
+    model : "Discovery",
+    combustionType : 'electric',
+    saftyApprovals : ['US Standard','Global Standard'],
     isReleased : true
 })
 
 // the async saves the data in the mongoDB by taking the actual model and data object
 
 async function createCar(Car, car){
-
+    try{
+    await car.validate() // inbuilt validation promise
     const result = await car.save()
     console.log("Response from mongoDB : ",result)
+    }
+    catch(error){
+        //console.error("Error occured while doing saving of the document ",error.message)
+        for(field in error.errors){
+            console.log(error.errors[field])
+        }
+    }
 }
 
-//createCar(Car, car)
+createCar(Car, car)
 
 
 // async method to retrive the documents form the mongodb
@@ -59,9 +74,10 @@ async function updateCar(Car, id){
 
 // async function to delete a document
 
+
 async function deleteCourse(Car, id){
     const car = await Car.findByIdAndDelete(id)
     console.log(car)
 }
 
-deleteCourse(Car, '65ad9c11631143c3290d01a3')
+//deleteCourse(Car, '65ad9c11631143c3290d01a3')
